81091bcf99213e12f2d6328d092172a2b63d5db0
vsesha <vasuops@gmail.com>
2016-01-30 12:08:14 -0600

    ver14

--- marginserver.js
+++ marginserver.js
@@ -12,7 +12,7 @@ var HTTP_404_ 			= require('./routes/HTTP_404')
 
 
 var expressApp = express();
-	expressApp.set('port', (process.env.SERVER_PORT || 6000));
+	expressApp.set('port', (process.env.PORT || 6000));
 	
 	expressApp.use(express.static(__dirname + '/'));
 
